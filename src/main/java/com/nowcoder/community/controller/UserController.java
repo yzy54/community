bce17ef3ab584fea0b2687ead1bd28980a62d0cf
yzy54 <993552232@qq.com>
2021-07-03 10:49:25 +0800

    第二次提交

---
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -0,0 +1,140 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import javafx.geometry.Pos;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @LoginRequired
+    @RequestMapping(value = "/setting", method = RequestMethod.GET)
+    public String getSettingPage(){
+        return "/site/setting";
+    }
+
+    @LoginRequired
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        if (headerImage == null) {
+            model.addAttribute("error", "您还没有选择图片");
+            return "/site/setting";
+        }
+
+        String filename = headerImage.getOriginalFilename();
+        String suffix = filename.substring(filename.lastIndexOf("."));
+        if(StringUtils.isBlank(suffix)){
+            model.addAttribute("error", "文件格式不正确");
+            return "/site/setting";
+        }
+
+        //生成随机文件名，并保留后缀
+        filename = CommunityUtil.generateUUID() + suffix;
+        //确定文件存放路径
+        File dest = new File(uploadPath + "/" + filename);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            log.error("上传文件失败" + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器发生异常", e);
+        }
+
+        //更新头像路径
+        //localhost:8080/community/user/header/xxx.jpg
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + filename;
+        userService.updateHeader(user.getId(), headerUrl);
+        return "redirect:/index";
+    }
+
+    @RequestMapping(value = "/header/{filename}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("filename") String filename, HttpServletResponse response){
+        // 服务器存放路径
+        filename = uploadPath + "/" + filename;
+        //文件后缀
+        String suffix = filename.substring(filename.lastIndexOf("."));
+        //响应图片
+        response.setContentType("image/" + suffix);
+        try(FileInputStream fis = new FileInputStream(filename);
+            ServletOutputStream os = response.getOutputStream();
+        ) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while((b=fis.read(buffer))!=-1){
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            log.error("读取头像失败" + e.getMessage());
+        }
+    }
+
+    @LoginRequired
+    @RequestMapping(value = "/updatePassword", method = RequestMethod.POST)
+    public String updatePassword(String oriPassword, String newPassword, Model model){
+        User user = hostHolder.getUser();
+
+        oriPassword = CommunityUtil.md5(oriPassword + user.getSalt());
+
+        //对原始密码进行判断
+        if(!user.getPassword().equals(oriPassword)){
+            model.addAttribute("PasswordError", "原始密码错误");
+            return "/site/setting";
+        }
+
+        if(newPassword == null) {
+            model.addAttribute("newPasswordError", "密码不能为空");
+            return "/site/setting";
+        }
+
+        //判断新密码是否符合要求大于8位
+        if(newPassword.length()<8){
+            model.addAttribute("newPasswordError", "密码不能小于8位");
+            return "/site/setting";
+        }
+
+        newPassword = CommunityUtil.md5(newPassword + user.getSalt());
+
+        userService.updatePassword(user.getId(), newPassword);
+        return "redirect:/index";
+    }
+
+
+}
---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,33 @@
+package com.nowcoder.community;
+
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        Context context = new Context();
+        context.setVariable("username", "jerry");
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+        mailClient.sendMail("993552232@qq.com", "html", content);
+    }
+}
